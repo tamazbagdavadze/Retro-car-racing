51e7cacadbb428529d6497c3a1f4514c27a42fcc
Tamaz Bagdavadze <tamazbagdavadze@gmail.com>
2015-12-31 04:59:54 -0800

    responsive square

--- js/js.js
+++ js/js.js
@@ -5,9 +5,67 @@
 
 var RetroCarRacing = (function(){
 
-    //TODO  private random shit
+    'use strict';
+
+    var domElement = null;
+    var ctx = null;
+    var screenWidth = null;
+    var screenHeight = null;
+
+    function drawSquare(x,y,width){
+        "use strict";
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 3;
+        ctx.strokeRect(x, y, width, width);
+
+        var innerSquareWidth = width/4;
+
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 1;
+
+        var from = innerSquareWidth / 2;
+        var to = width - from;
+
+        for(let i = from; i < to; i += innerSquareWidth){
+            for(let j = to - innerSquareWidth; j > from - innerSquareWidth ; j -= innerSquareWidth){
+                ctx.fillRect(x + i, y + j, innerSquareWidth, innerSquareWidth);
+                ctx.strokeRect(x + i, y + j, innerSquareWidth, innerSquareWidth);
+            }
+        }
+        
+    }
+
+    function resize(){
+        screenHeight = getComputedStyle(domElement)['height'].slice(0, -2);
+        screenWidth = getComputedStyle(domElement)['width'].slice(0, -2);
+        domElement.setAttribute('height', screenHeight);
+        domElement.setAttribute('width', screenWidth);
+    }
+
+    function init(){
+        ctx = domElement.getContext('2d');
+        resize();
+        window.addEventListener('resize',resize);
+    }
+
+    function render(){
+        requestAnimationFrame(render);
+    }
 
     return {
-        //TODO random shit
+        setDomElement : function(el){
+            domElement = el;
+        },
+        start : function(){
+            init();
+            drawSquare(30,30,60);
+        }
     };
 }());
+
+window.onload = function(){
+    "use strict";
+
+    RetroCarRacing.setDomElement(document.getElementById('canvas'));
+    RetroCarRacing.start();
+};
