914118f7e2ff82c059896d56f0eb6a9a00f45171
Tamaz Bagdavadze <tamazbagdavadze@gmail.com>
2015-12-31 08:10:17 -0800

    next step

--- js/js.js
+++ js/js.js
@@ -3,9 +3,21 @@
  */
 
 
-var RetroCarRacing = (function(){
+var RetroCarRacing = (function () {
 
     'use strict';
+
+    var Car = function (side, y) {
+        this.y = y;
+        this.side = side;
+    };
+
+    var sides = {
+        left: 0,
+        center: 1,
+        right: 2
+    };
+
 
     var domElement = null;
     var ctx = null;
@@ -13,8 +25,51 @@ var RetroCarRacing = (function(){
     var screenHeight = null;
     var squareWidth = null;
     var emptyRoadSquareNumber = 1;
+    var cars = [];
+    var myCar = null;
 
-    function drawSquare(x,y){
+
+    function drawCar(car) {
+
+        var x = squareWidth;
+        var y = car.y * squareWidth;
+
+        switch (car.side) {
+            case sides.left:
+            {
+                break;
+            }
+            case sides.center:
+            {
+                x += (3 * squareWidth);
+                break;
+            }
+            case sides.right:
+            {
+                x += (6 * squareWidth);
+                break;
+            }
+        }
+
+        drawSquare(x + squareWidth, y);
+
+        y += squareWidth / 1;
+
+        drawSquare(x, y);
+        drawSquare(x + squareWidth, y);
+        drawSquare(x + 2 * squareWidth, y);
+
+        y += squareWidth / 1;
+
+        drawSquare(x + squareWidth, y);
+
+        y += squareWidth / 1;
+
+        drawSquare(x, y);
+        drawSquare(x + 2 * squareWidth, y);
+    }
+
+    function drawSquare(x, y) {
         "use strict";
 
         var width = squareWidth;
@@ -23,7 +78,7 @@ var RetroCarRacing = (function(){
         ctx.lineWidth = 3;
         ctx.strokeRect(x, y, width, width);
 
-        var innerSquareWidth = width/4;
+        var innerSquareWidth = width / 4;
 
         ctx.strokeStyle = 'white';
         ctx.lineWidth = 1;
@@ -31,77 +86,120 @@ var RetroCarRacing = (function(){
         var from = innerSquareWidth / 2;
         var to = width - from;
 
-        for(let i = from; i < to; i += innerSquareWidth){
-            for(let j = to - innerSquareWidth; j > from - innerSquareWidth ; j -= innerSquareWidth){
+        for (let i = from; i < to; i += innerSquareWidth) {
+            for (let j = to - innerSquareWidth; j > from - innerSquareWidth; j -= innerSquareWidth) {
                 ctx.fillRect(x + i, y + j, innerSquareWidth, innerSquareWidth);
                 ctx.strokeRect(x + i, y + j, innerSquareWidth, innerSquareWidth);
             }
         }
     }
 
-    function resize(){
-
-        screenHeight = parseInt(getComputedStyle(domElement)['height'].slice(0, -2), 10);
-        screenWidth = screenHeight / 18 * 10;
+    function resize() { //TODO crashes when height is 0
 
         var bodyWidth = parseInt(getComputedStyle(document.body)['width'].slice(0, -2), 10);
 
-        if(bodyWidth < screenWidth){
+        if (bodyWidth < screenWidth) { //TODO fix width change
             screenWidth = bodyWidth;
-            screenHeight = screenWidth / 10 * 18;
+            screenHeight = screenWidth / 11 * 18;
+        } else {
+            screenHeight = parseInt(getComputedStyle(domElement)['height'].slice(0, -2), 10);
+            screenWidth = Math.floor(screenHeight / 18 * 11);
         }
+
+        squareWidth = Math.floor(screenWidth / 11);
+        screenWidth = 11 * squareWidth; // pixel perfect :v
 
         domElement.setAttribute('height', screenHeight);
         domElement.setAttribute('width', screenWidth);
-
-        clear();
-
-        squareWidth = Math.floor(screenWidth / 10);
     }
 
-    function init(){
+    function keyDown(e) {
+        switch (e.which) {
+
+            /* left */
+            case 37:{
+                if(myCar.side != sides.left){
+                    myCar.side--;
+                }
+                break;
+            }
+
+            /* up */
+            case 38: {
+
+                break;
+            }
+
+            /*right*/
+            case 39: {
+                if(myCar.side != sides.right){
+                    myCar.side++;
+                }
+                break;
+            }
+
+            /* down */
+            case 40:{
+
+                break;
+            }
+        }
+    }
+
+    function init() {
         ctx = domElement.getContext('2d');
         resize();
-        window.addEventListener('resize',resize);
-        render();
+        window.addEventListener('resize', resize);
+        window.addEventListener('keydown', keyDown);
 
-        setInterval(function(){
+        myCar = new Car(sides.right, 14);
+
+        render();
+        setInterval(function () {
             emptyRoadSquareNumber = ++emptyRoadSquareNumber % 4;
-        }, 100);
+        }, 200);
     }
 
-    function clear(){
+    function clear() {
         ctx.clearRect(0, 0, screenWidth, screenHeight);
     }
 
-    function render(){
+    function render() {
         clear();
         drawRoad();
+
+        drawCar(myCar);
+
         requestAnimationFrame(render);
     }
 
-    function drawRoad(){
+    function drawRoad() {
 
-        for(let i = 0; i < screenHeight / squareWidth; i++){
+        for (let i = 0; i < screenHeight / squareWidth; i++) {
 
-            if(i % 4 == emptyRoadSquareNumber)continue;
+            if (i % 4 == emptyRoadSquareNumber)
+                continue;
 
             drawSquare(0, i * squareWidth);
             drawSquare(screenWidth - squareWidth, i * squareWidth);
         }
+
+        //for(let i = 0; i< screenWidth / squareWidth; i++ ){
+        //    drawSquare(i * squareWidth, 0);
+        //}
     }
 
     return {
-        setDomElement : function(el){
+        setDomElement: function (el) {
             domElement = el;
         },
-        start : function(){
+        start: function () {
             init();
         }
     };
 }());
 
-window.onload = function(){
+window.onload = function () {
     "use strict";
 
     RetroCarRacing.setDomElement(document.getElementById('canvas'));
